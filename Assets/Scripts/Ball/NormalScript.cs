1e82bbcd1e61e1e037e8c7d96c680ea5f1862245
Nguyen Huy Hung <nguyenhuyhungframgia@gmail.com>
2014-07-16 17:15:40 -0700

    start project

---
+++ Assets/Scripts/Ball/NormalScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NormalScript : MonoBehaviour {
+	private int currentAnimation = 1;
+	// Use this for initialization
+	private Animator animator;
+	void Start () {
+		animator = gameObject.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(animator.GetCurrentAnimatorStateInfo(0).IsName("nameAnimation" + currentAnimation.ToString()) && animator.GetCurrentAnimatorStateInfo(0).normalizedTime > (float)0.8){
+			animator.SetBool ("bAnimation" + currentAnimation.ToString (), false);
+			GameObject.Find("GameManagement").transform.GetComponent<TouchHandler>().isFirst = true;
+		}
+	}
+
+	public void runAnimation(int indexAniamtion, int posBall){
+		switch (posBall) {
+			case 1:
+				break;
+			case 2:
+				break;
+			default: 
+				break;
+		}
+		animator.SetBool ("bAnimation" + indexAniamtion.ToString (), true);
+		currentAnimation = indexAniamtion;
+	}
+}
---
+++ Assets/Scripts/TouchHandler.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Net.Sockets;
+
+public class TouchHandler : MonoBehaviour {
+	public GameObject player;
+	private Animator animator;
+	public GameObject ball;
+	public bool isFirst = true;
+	// Use this for initialization
+	void Start () {
+		animator = player.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update (){
+		if ((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) || Input.GetAxis("Vertical") != 0) {
+			Debug.Log("Drag ended!");
+			animator.SetBool("bShoot", true);
+
+		}
+		if (isFirst) {
+			if(animator.GetCurrentAnimatorStateInfo(0).IsName("shooting") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime > (float)0.50){
+				isFirst = false;
+				ball.transform.GetComponent<NormalScript>().runAnimation(2, 0);
+			}
+		}
+		Debug.Log ("time " + animator.GetCurrentAnimatorStateInfo (0).normalizedTime);
+
+		if(animator.GetCurrentAnimatorStateInfo(0).IsName("shooting") && animator.GetCurrentAnimatorStateInfo(0).normalizedTime > (float)0.95){
+			animator.SetBool("bShoot", false);
+		}
+	}
+}
